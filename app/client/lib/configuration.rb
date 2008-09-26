0b4dbd49808b6183d0f250b49c96a912fdd28775
Jorrel Ang <jorrel@gmail.com>
2008-09-26 18:14:06 +0800

    Configuration can now be instantiated using a hash and can be saved to a gived file using #save_to

--- app/client/lib/configuration.rb
+++ app/client/lib/configuration.rb
@@ -11,15 +11,32 @@ class Configuration < OpenStruct
 
   delegate :merge!, :reverse_update, :reverse_merge!, :to => '@table'
 
-  def initialize(file)
-    self.file = file.respond_to?(:path) ? file.path : file
-    load_configuration
+  def initialize(argument = {})
+    if argument.is_a?(String) or argument.respond_to?(:path)
+      self.file = path_from argument
+      hash = configuration_from_file
+    elsif argument.is_a?(Hash)
+      hash = argument
+    else
+      raise ArgumentError, "invalid parameter: #{argument.inspect}"
+    end
+    load_configuration hash
   end
 
   #
-  # save the changes to the config to the yml file
+  # save the changes back to the config file
   #
   def save
+    raise "Cannot save, use #{self.class.name}#save_to and provide a filename" if file.nil?
+    save_to file
+  end
+
+  #
+  # save changes to a file
+  #
+  def save_to(file)
+    path = file.respond_to?(:path) ? file.path : file
+    self.file = path if self.file.nil? # further calls to 'save' will save to same file
     dump = recursively_stringify_keys_of(marshal_dump).to_yaml
     File.open(file, 'w') { |config_file| config_file.write dump }
   end
@@ -28,8 +45,16 @@ class Configuration < OpenStruct
 
   private
 
-    def load_configuration
-      marshal_load recursively_symbolize_keys_of( YAML.load( ERB.new( IO.read(file) ).result ) || {})
+    def load_configuration(hash)
+      marshal_load prepare_configuration(hash)
+    end
+
+    def prepare_configuration(hash)
+      recursively_symbolize_keys_of(hash)
+    end
+
+    def configuration_from_file
+      YAML.load( ERB.new( IO.read(file) ).result ) || {}
     end
 
     def recursively_symbolize_keys_of(hash)
@@ -44,6 +69,16 @@ class Configuration < OpenStruct
         h[k.to_s] = v.is_a?(Hash) ? recursively_stringify_keys_of(v) : v
         h
       }
+    end
+
+    def path_from(file)
+      if file.is_a?(String)
+        file
+      elsif file.respond_to?(:path)
+        file.path
+      else
+        raise ArgumentError, "Invalid file or file path: #{file}"
+      end
     end
 end
 
