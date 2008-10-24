02c270feb3a9242a1b5830c41e02e48a6c27a6ac
Jorrel Ang <jorrel@gmail.com>
2008-10-24 15:50:28 +0800

    started implementing students controller and student model

--- app/web/app/controllers/students_controller.rb
+++ app/web/app/controllers/students_controller.rb
@@ -1,5 +1,40 @@
 class StudentsController < ApplicationController
+  before_filter :find_student, :only => [:show, :edit, :destroy]
+
   def index
     @students = Student.find(:all)
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def show
+  end
+
+  def create
+    @student = Student.create! params[:student]
+  rescue ActiveRecord::RecordInvalid
+    flash[:warning] = 'Invalid Record'
+    render :action => 'new'
+  else
+    flash[:success] = 'New Student created'
+    redirect_to student_path(@student)
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+      def find_student
+        @student = Student.find_by_student_id(params[:id])
+        if not @student
+          flash[:warning] = "No student found with id: #{params[:id]}"
+          redirect_to '/students'
+        end
+      end
 end
--- app/web/app/models/student.rb
+++ app/web/app/models/student.rb
@@ -8,4 +8,27 @@ class Student < ActiveRecord::Base
     :awol                   =>  'W', # A.W.O.L.
     :alumni                 =>  'A'
   }
+
+  class << self
+    def find_with_student_id(*args, &block)
+      if args.size == 1 and String === args.first and args.first =~ /^\d{4}-\d{5}$/
+        find_by_student_id!(*args)
+      else
+        find_without_student_id(*args, &block)
+      end
+    end
+    alias_method_chain :find, :student_id
+
+    def find_by_student_id!(student_id)
+      if student = find_every(:conditions => {:student_id => student_id}).first
+        student
+      else
+        raise ActiveRecord::RecordNotFound, "No student with student id of #{student_id}"
+      end
+    end
+  end
+
+  def to_param
+    student_id || id
+  end
 end
