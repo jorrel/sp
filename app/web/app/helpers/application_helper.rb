e40e842fd252da462998c6ed9fbbd5b33a2de6c9
Jorrel Ang <jorrel@gmail.com>
2008-10-30 17:03:17 +0800

    added new helper method: table and started admin listing

--- app/web/app/helpers/application_helper.rb
+++ app/web/app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def table(html_options = {}, &block)
+    html_options.reverse_update(:cellspacing => 0, :cellpadding => 0)
+    content_tag(:table, html_options, &block)
+  end
 end
