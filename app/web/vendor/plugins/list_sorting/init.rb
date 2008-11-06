09b7f8fe4da91ff5c34f3619b032ad8818ed345f
Jorrel Ang <jorrel@gmail.com>
2008-11-06 11:57:42 +0800

    removed list_sorting plugin

--- app/web/vendor/plugins/list_sorting/init.rb
+++
@@ -1,2 +0,0 @@
-ActionController::Base.__send__(:include, ListSorting::Controller)
-ActionView::Base.__send__(:include, ListSorting::Helper)
--- app/web/vendor/plugins/list_sorting/tasks/list_sorting_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :list_sorting do
-#   # Task goes here
-# end
