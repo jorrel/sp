8a8e6a3b6d3738fe5fc45b11be6f1494c52de5b0
Jorrel Ang <jorrel@gmail.com>
2008-11-17 17:17:49 +0800

    improved looks for the login page

--- app/web/lib/authenticated_system.rb
+++ app/web/lib/authenticated_system.rb
@@ -64,11 +64,12 @@ module AuthenticatedSystem
     def access_denied
       respond_to do |format|
         format.html do
+          flash[:warning] = 'Please login to continue'
           store_location
           redirect_to new_session_path
         end
         # format.any doesn't work in rails version < http://dev.rubyonrails.org/changeset/8987
-        # Add any other API formats here.  (Some browsers, notably IE6, send Accept: */* and trigger 
+        # Add any other API formats here.  (Some browsers, notably IE6, send Accept: */* and trigger
         # the 'format.any' block incorrectly. See http://bit.ly/ie6_borken or http://bit.ly/ie6_borken2
         # for a workaround.)
         format.any(:json, :xml) do
@@ -114,7 +115,7 @@ module AuthenticatedSystem
         self.current_admin = Admin.authenticate(login, password)
       end
     end
-    
+
     #
     # Logout
     #
@@ -149,7 +150,7 @@ module AuthenticatedSystem
       logout_keeping_session!
       reset_session
     end
-    
+
     #
     # Remember_me Tokens
     #
@@ -161,25 +162,25 @@ module AuthenticatedSystem
 
     def valid_remember_cookie?
       return nil unless @current_admin
-      (@current_admin.remember_token?) && 
+      (@current_admin.remember_token?) &&
         (cookies[:auth_token] == @current_admin.remember_token)
     end
-    
+
     # Refresh the cookie auth token if it exists, create it otherwise
     def handle_remember_cookie!(new_cookie_flag)
       return unless @current_admin
       case
       when valid_remember_cookie? then @current_admin.refresh_token # keeping same expiry date
-      when new_cookie_flag        then @current_admin.remember_me 
+      when new_cookie_flag        then @current_admin.remember_me
       else                             @current_admin.forget_me
       end
       send_remember_cookie!
     end
-  
+
     def kill_remember_cookie!
       cookies.delete :auth_token
     end
-    
+
     def send_remember_cookie!
       cookies[:auth_token] = {
         :value   => @current_admin.remember_token,
