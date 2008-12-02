646f56f55c29f54578896e885276493d17ff3d25
Jorrel Ang <jorrel@gmail.com>
2008-12-02 20:43:50 +0800

    Focus first invalid form field
    If there are any invalid fields (the user had validation error(s)
    and was just presented the form again) then focus on the first
    invalid field instead of the first field if any invalid fields
    exists in the first form.

--- app/web/public/javascripts/application.js
+++ app/web/public/javascripts/application.js
@@ -22,8 +22,13 @@ var focusOnFirstFormElement = function() {
   var scope = $('content') || document.body;
   var forms = scope.getElementsBySelector('form');
   if(forms.any()) {
-    var fields = forms[0].getElementsBySelector('input[type=text]');
-    if(fields.any()) fields[0].select();
+    var fieldsWithError = forms[0].getElementsBySelector('span.field-with-errors input[type=text]');
+    if(fieldsWithError.length > 0)
+      fieldsWithError[0].select();
+    else {
+      var fields = forms[0].getElementsBySelector('input[type=text]');
+      if(fields.any()) fields[0].select();
+    }
   }
 }
 focusOnFirstFormElement.runOnLoad();
