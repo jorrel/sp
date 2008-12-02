50262807a8ba141c37a37bfaa9cc0cd79b4636db
Jorrel Ang <jorrel@gmail.com>
2008-12-02 19:51:01 +0800

    dashboard controller test

--- app/web/test/functional/dashboard_controller_test.rb
+++ app/web/test/functional/dashboard_controller_test.rb
@@ -1,8 +1,18 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+require 'dashboard_controller'
 
 class DashboardControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_index
+    as_superadmin
+    get :index
+    assert_response :success
   end
+
+  def test_index_as_non_super
+    login_as :aaron
+    get :index
+    assert_response :success
+  end
+
 end
